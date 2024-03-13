301ed4657c29447cd71c662848e4c121ba62e215
Thomas Noire <booking.bogdan@gmail.com>
2024-03-13 13:50:43 +0200

    Third Task

---
+++ Pr_3/Modules/Point.cs
@@ -0,0 +1,16 @@
+﻿namespace Pr_3.Modules
+{
+    internal class Point
+    {
+        public int X { get; }
+        public int Y { get; }
+        public string Name { get; }
+
+        public Point(int x, int y, string name)
+        {
+            X = x;
+            Y = y;
+            Name = name;
+        }
+    }
+}
